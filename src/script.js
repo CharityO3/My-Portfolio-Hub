578a8d85784d407aa968e226a5eb4cd02d412bda
Charity Orhoridiohwo <79048698+CharityO3@users.noreply.github.com>
2025-01-27 21:18:10 +0100

    Updated script.js file

--- src/script.js
+++ src/script.js
@@ -4,15 +4,3 @@ new Typewriter("h1", {
   cursor:"",
   delay:30,
 });  
-
-function displayEmail(event){
-  let emailElement = document.querySelector("#email-link");
-  emailElement.classList.add("custom-border", "generating");
-  setTimeout(() => {
-    emailElement.classList.remove("generating");
-  }, 6000); // 5000ms = 5 seconds
-}
-  
-
-let contactLink = document.querySelector("#contact");
-contactLink.addEventListener("click", displayEmail);
